87e20d5d950a5b1c9217e3179ca5f7ee5917364f
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-09-28 12:03:51 -0800

    Edited js file

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 alert("Welcome to the our game.");
 
-let part1=prompt("Your turn! Enter 'R' for 'Rock', 'P' for 'Paper' or 'S' for 'Scissors'. ");
-if (part1==="R"){
+let part1 = prompt("Your turn! Enter 'R' for 'Rock', 'P' for 'Paper' or 'S' for 'Scissors'. ");
+if (part1 === "R") {
     alert("You selected 'ROCK'.");
 }
-else if(part1==="P"){
+else if (part1 === "P") {
     alert("You selected 'PAPER'.");
 }
-else if(part1==="S"){
+else if (part1 === "S") {
     alert("You selected 'SCISSORS'.");
-} 
-else{
+}
+else {
     alert("TRY AGAIN! YOU MUST ENTER AN 'R', 'P' or 'S' .");
 }
 
 confirm("Now it is the computer turn! Hit 'OK' to continue.");
 
-var options = ['R', 'P', 'S'];
+var options = ["R", "P", "S"];
 
 var computerChoice = options[Math.floor(Math.random() * options.length)];
 
 confirm("The computer chose " + computerChoice);
 
-function whoWon (part1, computerChoice) {
+function whoWon(part1, computerChoice) {
 
-    if (part1===computerChoice) {
-        return "Tie!"
+
+    if (part1 == "R" && computerChoice == "P") {
+        return alert("Computer wins!!");
+
+    } else if (part1 == "R" && computerChoice == "S") {
+        return alert("You win!!");
     }
-    
-    if (part1 === "R" && computerChoice === "P") {
-                return "Computer wins!!"
-            }
 
-            else if (part1 === "R" && computerChoice === "S") {
-                return "You win!!"
-            }
+    else if (part1 == "P" && computerChoice == "R") {
+        return alert("You win!!");
+    }
 
-            else if (part1 === "P" && computerChoice === "R") {
-                return "You win!!"
-            }
+    else if (part1 == "P" && computerChoice == "S") {
+        return alert("Computer wins!!");
+    }
 
-            else if (part1 === "P" && computerChoice === "S") {
-                return "Computer wins!!"
-            }
+    else if (part1 == "S" && computerChoice == "R") {
+        return alert("Computer wins!!");
+    }
 
-            else if (part1 === "S" && computerChoice === "R") {
-                return "Computer wins!!"
-            }
+    else if (part1 == "S" && computerChoice == "P") {
+        return alert("You win!!");
+    }
+    else {
+        return alert("Tie!");
 
-            else if (part1 === "S" && computerChoice === "P") {
-                return "You win!!"
-            }
+    }
 }
 
-confirm (whoWon);
+whoWon();
+confirm("Do you want to play another round?");
